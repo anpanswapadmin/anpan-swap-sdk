8fc8feb0768a993a9856d70de561e71494f6080c
anpanswap <anpanswap@gmail.com>
2021-09-17 08:53:54 +0700

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  BSCTESTNET = 97
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xEb4D933D56E5deF7B24F543766dD398f1DaFEEd5'
+
+export const INIT_CODE_HASH = '0x964872d098c77cc1e61eee3279aec45402a1f847306c38345d7e44bd1a855827'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _998 = JSBI.BigInt(998)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ test/constants.test.ts
@@ -0,0 +1,20 @@
+import { INIT_CODE_HASH } from '../src/constants'
+
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+// this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
+// and load the JSON.
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+
+describe('constants', () => {
+  describe('INIT_CODE_HASH', () => {
+    // it('matches computed bytecode hash', () => {
+    //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+    // })
+    //TODO: Replace with real AnpansPair.json
+    it('matches computed bytecode hash', () => {
+      expect(INIT_CODE_HASH).toEqual('0x964872d098c77cc1e61eee3279aec45402a1f847306c38345d7e44bd1a855827')
+    })
+  })
+})
